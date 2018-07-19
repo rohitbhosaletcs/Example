48e6b9160449c59808676359f42973259df78708
rohitbhosaletcs <bhosale111rohit@gmail.com>
2018-07-19 23:13:11 +0530

    Created GiExampl project

---
+++ GitExample/src/com/example/First.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class First {
+public static void main(String args[]){
+System.out.println("First Git Example");
+}
+}
---
+++ GitExample/src/com/example1/Second.java
@@ -0,0 +1,10 @@
+package com.example1;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Git Example");
+	}
+
+}
