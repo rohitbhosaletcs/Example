c7d15e57ca1701724fa1af16db952acda2f921bc
rohitbhosaletcs <bhosale111rohit@gmail.com>
2018-07-22 21:20:39 +0530

    Added NewGitMavenExample to repo

---
+++ NewGitMavenExample/src/test/java/com/example/FirstTest.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@Test
+	public void firstTest(){
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void secondTest(){
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void thirdTest(){
+		Assert.assertTrue(true);
+	}
+
+
+}
